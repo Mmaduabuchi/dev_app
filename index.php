1b9aad20cb8dd33bbd557415275b5838281975d5
mmaduabuchi <emmanuelukwe1@gmail.com>
2025-03-29 14:54:36 +0100

    started a project

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+
+<body>
+
+    <a href="http://localhost/dev%20app/we-welcome-you/">
+        <button>Home page</button>
+    </a>
+    <a href="http://localhost/dev%20app/login/">
+        <button>sigin page</button>
+    </a>
+    <a href="http://localhost/dev%20app/register/">
+        <button>signup page</button>
+    </a>
+
+</body>
+
+</html>
